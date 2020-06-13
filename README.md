6a2a52f2e8111cce386377c19705222d4eb02184
8696 <204084802@qq.com>
2020-06-14 01:32:04 +0800

    Sun Jun 14 2020 01:32:04 GMT+0800 (China Standard Time)

--- README.md
+++ README.md
@@ -13,12 +13,19 @@ const diff = require('version-diff');
 ```
 #### 接口
 
-- diff：Function
+- diff: Function
     - param
         + rangeV: Array\<min: String, max: String\> 版本范围，例如 ['0.0.1', '1.0.0'] 表示从0.0.1至1.0.0之间可匹配。包含0.0.1、但不包括1.0.0
         + diffV: String 被对比版本
     - return
         + Boolean
+
+- diff.main: Function
+    - param
+        + v1: String | 比较的版本
+        + v2: String | 被比较的版本
+    - return
+        + String | '<': v1 小于 v2 、'=': v1 等于 v2 、'>': v1 大于 v2
 
 ###### 例子
 
@@ -58,4 +65,15 @@ console.log(diff(['1.0','2.0'], '1.234'));           // true
 console.log(diff(['1.0.0.0','2.0.0.0'], '1'));       // true
 console.log(diff(['1.0.0.0','2.0.0.0'], '1.6.7.8')); // true
 console.log(diff(['1.0.0.0','2.0.0.0'], '2'));       // false
+console.log(diff(['1.0.0.0','1'], '1'));             // true
+```
+
+- diff.main
+
+```javascript
+console.log(diff.main('1.0', '1.1'));             // <
+console.log(diff.main('2.0', '1.1'));             // >
+console.log(diff.main('2.0', '2.0'));             // =
+console.log(diff.main('2', '2.0.0.0'));           // =
+console.log(diff.main('2.0.0.1.0.1', '2.0.0.1')); // >
 ```
--- test/001.js
+++ test/001.js
@@ -1,40 +1,35 @@
 const diff = require('../src/index');
 
+console.log(diff(['1', '2'], '1.2'));             // true
+console.log(diff(['1.0', '2.0'], '1.2.345'));     // true
+console.log(diff(['1.0.0', '2.0.0'], '1.0.2'));  // true
+console.log(diff(['1.0.0', '2.0.0'], '0.0.12')); // false
+// 包括 1.0.0
+console.log(diff(['1.0.0', '2.0.0'], '1.0.0'));  // true
+// 不包括 2.0.0
+console.log(diff(['1.0.0', '2.0.0'], '2.0.0'));  // false
 
-console.log(diff(['*', '0.0.1'], '0.0.0'));
-console.log(diff(['0.0.1', '1.0.0'], '0.0.1'));
-console.log(diff(['1.0.0', '2.0.0'], '1.0.1234'));
-console.log(diff(['1.0.0', '2.0.0'], '1.23.1234'));
-console.log(diff(['1.0.0', '*'], '12.23.1234'));
-console.log(diff(['1.0.0', '1.0.0'], '1.0.0'));
-console.log(diff(['*', '*'], '1.0.0'));
-console.log(diff(['*'], '1.0.0'));
-
-console.log(diff(['1.0.0','2.0.0'], '1.324'));
-console.log(diff(['1.0.0', '2.0.0'], '0.0.1234'));
-// console.log(diff(['1.0.0', '2.0.0'], '0.0.12'))
-// console.log(diff(['*', '2.0.0'], '2.0.2'));
-console.log(diff(['1.0.0', '2.0.0'], '1.0.0'));
-console.log(diff(['*', '2.0.1'], '2.0.0'));
+console.log(diff(['*', '2.0.0'], '1.0.2')); // true
 console.log(diff(['*', '2.0.0'], '2.0.0')); // false
+
+
 console.log(diff(['1.0.0', '*'], '1.0.2')); // true
-console.log(diff(['1.0.0', '*'], '0.2.0'));
-console.log(diff(['*'], '1.0.2'));
+console.log(diff(['1.0.0', '*'], '0.2.0')); // false
 
-console.log(diff(['1','2'], '1.2'));
-console.log(diff(['1.0','2.0'], '1.2')); // true
-console.log(diff(['*','*'], '1.0.2'));   // true
 
-console.log(diff(['1.0','2.0'], '1.234'));
-console.log(diff(['*','2.0'], '1.6666666.0'));
-console.log(diff(['*','2.0.0.0.0'], '1.2134.2.3.4.5.5'));
-console.log(diff(['1.0','*'], '1.0.2'));
-console.log(diff(['1.0.0.0','2.0.0.0'], '1'));
-console.log(diff(['1.0.0.0','2.0.0.0'], '1.6.7.8'));
-console.log(diff(['1.0.0.0','2.0.0.0'], '2'));
 console.log(diff(['*'], '1.0.2'));                   // true
-console.log(diff(['*','*'], '1.0.2'));               // true
-console.log(diff(['1.0','2.0'], '1.234'));           // true
-console.log(diff(['1.0.0.0','2.0.0.0'], '1'));       // true
-console.log(diff(['1.0.0.0','2.0.0.0'], '1.6.7.8')); // true
-console.log(diff(['1.0.0.0','2.0.0.0'], '2'));       // false
+console.log(diff(['*', '*'], '1.0.2'));               // true
+console.log(diff(['1.0', '2.0'], '1.234'));           // true
+console.log(diff(['1.0.0.0', '2.0.0.0'], '1'));       // true
+console.log(diff(['1.0.0.0', '2.0.0.0'], '1.6.7.8')); // true
+console.log(diff(['1.0.0.0', '2.0.0.0'], '2'));       // false
+
+console.log(diff(['1.0.0.0.0', '1.0.0.3.0'], '1.000000.00.3'));  // false
+console.log(diff(['1.0.0.0', '1'], '1')); // true
+
+
+console.log(diff.main('1.0', '1.1'));             // <
+console.log(diff.main('2.0', '1.1'));             // >
+console.log(diff.main('2.0', '2.0'));             // =
+console.log(diff.main('2', '2.0.0.0'));           // =
+console.log(diff.main('2.0.0.1.0.1', '2.0.0.1')); // >
